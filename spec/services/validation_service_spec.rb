80ebd204514a8e9e958dbac126b2d1efe24e7192
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-09-12 11:52:40 -0600

    Tried to set up webmock, it was breaking all the other tests

--- spec/services/validation_service_spec.rb
+++ spec/services/validation_service_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
+# require 'webmock/rspec'
 
 describe ValidationService do
   it 'can make calls and indicate when it gets a 404' do
     word_1 = 'notaword'
+    # json_response_1 = File.open('./fixtures/notaword.json')
+    # stub_request(:get, "https://od-api.oxforddictionaries.com/api/v1/inflections/en/#{word_1}").to_return(status: 404, body: json_response_1)
 
     response_1 = ValidationService.new(word_1).validation
 
@@ -10,7 +13,8 @@ describe ValidationService do
     expect(response_1).to_not respond_to(:keys)
 
     word_2 = 'hello'
-
+    # json_response_2 = File.open('./fixtures/hello.json')
+    # stub_request(:get, "https://od-api.oxforddictionaries.com/api/v1/inflections/en/#{word_2}").to_return(status: 200, body: json_response_2)
     response_2 = ValidationService.new(word_2).validation
 
     expect(response_2).to respond_to(:keys)
